bdfdc782182993a518a22684e4317e3d742ba5e2
NightBrownie <NightBrownie>
2013-11-11 19:02:30 +0300

    Updated Section vm, section highlight added.

--- Tester/ViewModels/SectionViewModel.cs
+++ Tester/ViewModels/SectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,61 @@ namespace Tester.ViewModels
 {
     public class SectionViewModel : Conductor<object>
     {
-        private Brush TheorySectionBrush = new SolidColorBrush(Color.FromArgb(255, 173, 255, 107));
-        private Brush TestSectionBrush = new SolidColorBrush(Color.FromArgb(255, 219, 112, 147));
-        private Brush PracticeSectionBrush = new SolidColorBrush(Color.FromArgb(255, 30, 144, 47));
+        private Brush _currentBorderBrush;
+        private Brush _theorySectionBrush;
+        private Brush _testSectionBrush;
+        private Brush _practiceSectionBrush;
+
+        public Brush TheorySectionBrush
+        {
+            get { return _theorySectionBrush; }
+            set
+            {
+                _theorySectionBrush = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("TheorySectionBrush"));
+            }
+        }
+
+        public Brush TestSectionBrush
+        {
+            get { return _testSectionBrush; }
+            set
+            {
+                _testSectionBrush = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("TestSectionBrush"));
+            }
+        }
+
+        public Brush PracticeSectionBrush
+        {
+            get { return _practiceSectionBrush; }
+            set
+            {
+                _practiceSectionBrush = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("PracticeSectionBrush"));
+            }
+        }
 
         public Section Section { get; set; }
-        public Brush CurrentBorderBrush { get; set; }
+
+        public Brush CurrentBorderBrush
+        {
+            get { return _currentBorderBrush; }
+            set
+            {
+                _currentBorderBrush = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CurrentBorderBrush"));
+            }
+        }
 
         public SectionViewModel(Section section)
         {
+            TheorySectionBrush = new SolidColorBrush(Colors.GreenYellow);
+            TestSectionBrush = new SolidColorBrush(Colors.OrangeRed);
+            PracticeSectionBrush = new SolidColorBrush(Colors.LightSkyBlue);
+
             Section = section;
-            CurrentBorderBrush = TestSectionBrush;
+            ShowTheoryButton();
         }
 
         public void ShowTheoryButton()
@@ -38,7 +83,7 @@ namespace Tester.ViewModels
             CurrentBorderBrush = TestSectionBrush;
         }
 
-        public void ShowPractiseButton()
+        public void ShowPracticeButton()
         {
             CurrentBorderBrush = PracticeSectionBrush;
         }
