f17948f4d97d5b3a34b24eb9a305cf2f10d83701
Nikita Klyushnikov <vipkonst@gmail.com>
2013-11-11 09:43:55 +0300

    Border added to SectionView to show current subsection (theory, test, practice).

--- Tester/ViewModels/SectionViewModel.cs
+++ Tester/ViewModels/SectionViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Caliburn.Micro;
 using Tester.Data;
 using Tester.Views;
@@ -12,25 +13,34 @@ namespace Tester.ViewModels
 {
     public class SectionViewModel : Conductor<object>
     {
+        private Brush TheorySectionBrush = new SolidColorBrush(Color.FromArgb(255, 173, 255, 107));
+        private Brush TestSectionBrush = new SolidColorBrush(Color.FromArgb(255, 219, 112, 147));
+        private Brush PracticeSectionBrush = new SolidColorBrush(Color.FromArgb(255, 30, 144, 47));
+
         public Section Section { get; set; }
+        public Brush CurrentBorderBrush { get; set; }
 
         public SectionViewModel(Section section)
         {
             Section = section;
+            CurrentBorderBrush = TestSectionBrush;
         }
 
         public void ShowTheoryButton()
         {
             //TODO: implements theory viewmodel in near future
+            CurrentBorderBrush = TheorySectionBrush;
         }
 
         public void ShowTestButton()
         {
             ActivateItem(new TestViewModel(Section.Test));
+            CurrentBorderBrush = TestSectionBrush;
         }
 
         public void ShowPractiseButton()
         {
+            CurrentBorderBrush = PracticeSectionBrush;
         }
     }
 }
