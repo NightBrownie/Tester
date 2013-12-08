12854c791c7ca850f9013dc7120f9cf04c15228f
NightBrownie <NightBrownie>
2013-12-08 19:37:14 +0300

    PracticeCrosswordView.xaml.cs updated afrer merge with Anarjells gist. Implemented horizontal/vertical tabulation through crossword, depends on previous word cell.

--- Tester/Views/PracticeCrosswordView.xaml.cs
+++ Tester/Views/PracticeCrosswordView.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Tester.Data;
 
 namespace Tester.Views
@@ -20,7 +13,19 @@ namespace Tester.Views
     {
         private Crossword crossword;
         private TextBox[,] boxes;
-        private Dictionary<Crossword.Word, List<TextBox>> wordStrips = new Dictionary<Crossword.Word, List<TextBox>>();
+        private readonly Dictionary<Crossword.Word, List<TextBox>> wordStrips = new Dictionary<Crossword.Word, List<TextBox>>();
+        private bool isPrevVertical;
+
+        private static readonly List<Key> AllowedKeys = new List<Key>
+            {
+                Key.OemMinus, // -
+                Key.OemOpenBrackets, // Х
+                Key.OemCloseBrackets, // Ъ
+                Key.OemSemicolon, // Ж
+                Key.OemQuotes, // Э
+                Key.OemComma, // Б
+                Key.OemPeriod, // Ю
+            };
 
         public PracticeCrosswordView()
         {
@@ -31,6 +36,7 @@ namespace Tester.Views
         {
             this.crossword = crossword;
             boxes = new TextBox[64, 64];
+            isPrevVertical = false;
 
             foreach (var word in crossword.Words)
             {
@@ -95,14 +101,61 @@ namespace Tester.Views
         void box_KeyUp(object sender, KeyEventArgs e)
         {
             var box = sender as TextBox;
+
+            if (box == null)
+                throw new NoNullAllowedException("box");
+
             if (box.Text.Length > 0)
                 box.Text = box.Text.Substring(box.Text.Length - 1).ToUpper();
             box.SelectAll();
             UpdateState();
-            for (int y = 0; y < boxes.GetLength(0); y++)
-                for (int x = 0; x < boxes.GetLength(0) - 1; x++)
-                    if (boxes[x, y] == box && boxes[x + 1, y] != null)
-                        boxes[x + 1, y].Focus();
+
+            var xCoord = 0;
+            var yCoord = 0;
+
+            //Поиск бокса
+            for (var y = 0; y < boxes.GetLength(0); ++y)
+            {
+                var breakFlag = false;
+                for (var x = 0; x < boxes.GetLength(0) - 1; ++x)
+                    if (Equals(boxes[x, y], box))
+                    {
+                        xCoord = x;
+                        yCoord = y;
+
+                        breakFlag = true;
+                        break;
+                    }
+                if (breakFlag)
+                    break;
+            }
+
+            //Проверка на первый символ слова
+            if (boxes[xCoord, yCoord - 1] == null && boxes[xCoord - 1, yCoord] == null)
+                isPrevVertical = boxes[xCoord, yCoord + 1] != null;
+
+            //Проверки на последний символ
+            if (isPrevVertical && boxes[xCoord, yCoord + 1] == null)
+                isPrevVertical = false;
+
+            if (!isPrevVertical && boxes[xCoord + 1, yCoord] == null)
+                isPrevVertical = true;
+
+            //Действие в зависимости от нажатой кнопки
+            if (e.Key >= Key.A && e.Key <= Key.Z || AllowedKeys.Contains(e.Key))
+            {
+                if (boxes[xCoord, yCoord + 1] != null && isPrevVertical)
+                    boxes[xCoord, yCoord + 1].Focus();
+                if (boxes[xCoord + 1, yCoord] != null && !isPrevVertical)
+                    boxes[xCoord + 1, yCoord].Focus();
+            }
+            else if (e.Key == Key.Back)
+            {
+                if (boxes[xCoord, yCoord - 1] != null && isPrevVertical)
+                    boxes[xCoord, yCoord - 1].Focus();
+                if (boxes[xCoord - 1, yCoord] != null && !isPrevVertical)
+                    boxes[xCoord - 1, yCoord].Focus();
+            }
         }
     }
 }
